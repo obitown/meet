524853591c6f2585f02f33c6267c0b6c00da3cc9
obitown <92471740+obitown@users.noreply.github.com>
2022-03-03 14:02:21 -0800

    retry

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,15 +1,18 @@
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
+
 /**
  * SCOPES allows you to set access levels; this is set to readonly for now because you don't have access rights to
- * update the calendar yourself. For more info, check out the SCOPES documentation at this link: https://developers.google.com/identity/protocols/oauth2/scopes
+ * update the calendar yourself. For more info, check out the SCOPES documentation at this 
+ * link: https://developers.google.com/identity/protocols/oauth2/scopes
  */
 const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
 /**
  * Credentials are those values required to get access to your calendar. If you see “process.env” this means
- * the value is in the “config.json” file. This is a best practice as it keeps your API secrets hidden. Please remember to add “config.json” to your “.gitignore” file.
+ * the value is in the “config.json” file. This is a best practice as it keeps your API secrets hidden.
+ * Please remember to add “config.json” to your “.gitignore” file.
  */
 const credentials = {
   client_id: process.env.CLIENT_ID,
@@ -30,19 +33,15 @@ const oAuth2Client = new google.auth.OAuth2(
 );
 
 /**
- *
  * The first step in the OAuth process is to generate a URL so users can log in with
  * Google and be authorized to see your calendar. After logging in, they’ll receive a code
  * as a URL parameter.
- *
  */
 module.exports.getAuthURL = async () => {
   /**
-   *
    * Scopes array passed to the `scope` option. Any scopes passed must be enabled in the
    * "OAuth consent screen" settings in your project on your Google Console. Also, any passed
-   *  scopes are the ones users will see when the consent screen is displayed to them.
-   *
+   * scopes are the ones users will see when the consent screen is displayed to them.
    */
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
@@ -52,7 +51,7 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "access-contorl-allow-origin": "*"
     },
     body: JSON.stringify({
       authUrl: authUrl,
