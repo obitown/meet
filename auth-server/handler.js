7e60cc91f6445ffe69ee75f2fa5a8986bd0fbba3
obitown <92471740+obitown@users.noreply.github.com>
2022-03-02 17:18:41 -0800

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,21 +1,3 @@
-'use strict';
-
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
