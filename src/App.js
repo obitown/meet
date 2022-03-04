cdf187ec72ce0e4ec5218298e4e965e061ab8221
obitown <92471740+obitown@users.noreply.github.com>
2022-03-04 14:57:32 -0800

    add citySearch and pass test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import EventList from './EventList';
+import CitySearch from './CitySearch';
 
 function App() {
   return (
     <div className="App">
       <EventList />
+      <CitySearch />
     </div>
   );
 }
